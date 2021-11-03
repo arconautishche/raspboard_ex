307bec372e44c5c710790c20dcc78033f9e16132
arconautishche <anton.iar@hey.com>
2021-11-03 19:29:19 +0100

    unreachable lights are not clickable

--- lib/raspboard_ex_web/components/light_components.ex
+++ lib/raspboard_ex_web/components/light_components.ex
@@ -3,7 +3,7 @@ defmodule RaspboardWeb.LightComponents do
 
   def light_switch(assigns) do
     ~H"""
-    <button
+    <div
       id={"light-switch-#{@id}"}
       class={"
         w-32
@@ -12,54 +12,38 @@ defmodule RaspboardWeb.LightComponents do
         shadow-md backdrop-blur
         flex flex-col justify-center place-items-end overflow-hidden
         cursor-pointer"}
-      phx-click="toggleLight" phx-value-lightid={@id}
+      {phx_click_attrs(assigns)}
       >
-      <div
-          class={"h-24 w-full
-            transition-all duration-500
-            #{classes(assigns).switch}
-            pt-2"}
-            >
-          <div
-              class={"w-full
-                text-center #{classes(assigns).name} text-xl font-bold text-opacity-20"}>
+        <div
+            class={"h-24 w-full
+              transition-all duration-500
+              #{classes(assigns).switch}
+              pt-2"}
+              >
+            <div
+                class={"w-full
+                  text-center #{classes(assigns).name} text-xl font-bold text-opacity-20"}>
 
-              <%= "#{@name}" %>
-          </div>
-      </div>
-      <div
-          class={"w-full h-5
-            cursor-pointer
-            border-t border-gray-400 border-opacity-25
-            #{classes(assigns).source}"}
-          style='background: linear-gradient(90deg, {$lightSourceBgEdges} 0%, {$lightSourceBgMidPoint} 50%, {$lightSourceBgEdges} 100%)'
-          class:light-source-on='{on}'>
-      </div>
+                <%= "#{@name}" %>
+            </div>
+        </div>
+        <div
+            class={"w-full h-5
+              cursor-pointer
+              border-t border-gray-400 border-opacity-25
+              #{classes(assigns).source}
+              transition-all duration-200"}
+            style='background: linear-gradient(90deg, {$lightSourceBgEdges} 0%, {$lightSourceBgMidPoint} 50%, {$lightSourceBgEdges} 100%)'>
+        </div>
 
-      </button>
+      </div>
     """
-  end
-
-  defp light_source_classes(%{on: true}) do
-    "bg-yellow-200"
-  end
-
-  defp light_source_classes(%{on: false}) do
-    "bg-gray-700"
-  end
-
-  defp light_switch_classes(%{on: true}) do
-    "bg-gray-100"
-  end
-
-  defp light_switch_classes(%{on: false}) do
-    "bg-gray-600"
   end
 
   defp classes(%{on: true}) do
     %{
       source: "bg-yellow-200",
-      switch: "bg-gray-100",
+      switch: "bg-yellow-50",
       name: "text-gray-800"
     }
   end
@@ -70,6 +54,14 @@ defmodule RaspboardWeb.LightComponents do
       switch: "bg-gray-600",
       name: "text-white"
     }
+  end
+
+  defp phx_click_attrs(%{disabled: true}) do
+    %{}
+  end
+
+  defp phx_click_attrs(%{id: id}) do
+    %{"phx-click": "toggleLight", "phx-value-lightid": id}
   end
 
 end
