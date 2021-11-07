f42a1b8347db1a1a818fbf103592f23bfbd38973
arconautishche <anton.iar@hey.com>
2021-11-07 15:00:47 +0100

    added a clock

--- lib/raspboard_ex_web/live/home_live.ex
+++ lib/raspboard_ex_web/live/home_live.ex
@@ -3,16 +3,7 @@ defmodule RaspboardExWeb.HomeLive do
 
   @impl true
   def mount(_params, _session, socket) do
-    if connected?(socket), do: Process.send_after(self(), :tick, 1_000)
-
-    {:ok, assign(socket, :temp, 42)}
-  end
-
-  @impl true
-  def handle_info(:tick, socket) do
-    Process.send_after(self(), :tick, 1_000)
-
-    {:noreply, assign(socket, :temp, socket.assigns.temp + 1)}
+    {:ok, socket}
   end
 
 
